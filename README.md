2e19667f3d82e263eb5938e80218a8608078f431
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2023-11-30 20:09:44 +0200

    Add dive install script
    Signed-off-by: Georgios Ntoutsos <gntouts@nubificus.co.uk>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # oneliners
 Collection of Bash scripts to perform various commonly used tasks
+
+## dive
+
+To install [dive](https://github.com/wagoodman/dive):
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/gntouts/oneliners/main/dive.sh | bash
+```
---
+++ dive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Checking latest version for https://github.com/wagoodman/dive..."
+DIVE_VERSION=$(curl -L -s -o /dev/null -w '%{url_effective}' "https://github.com/wagoodman/dive/releases/latest" | grep -oP "v\d+\.\d+\.\d+" | sed 's/v//')
+echo "Found latest version $DIVE_VERSION"
+CPU_ARCH=$(dpkg --print-architecture)
+echo "Downloading Debian package for dive v$DIVE_VERSION $CPU_ARCH"
+curl -L -o dive.deb https://github.com/wagoodman/dive/releases/download/v$DIVE_VERSION/dive_$DIVE_VERSION\_linux_$CPU_ARCH.deb
+
+sudo echo "Installing package..."
+sudo dpkg -i ./dive.deb
+
+sudo apt-get install -f
+rm -fr ./dive.deb
+echo ""
+echo "Success"
