142ae8c02390eb74eb2a5af07372c746036be873
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2023-12-04 17:48:32 +0200

    Add tldr script
    Signed-off-by: Georgios Ntoutsos <gntouts@nubificus.co.uk>

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ To install [dive](https://github.com/wagoodman/dive):
 ```bash
 curl -fsSL https://raw.githubusercontent.com/gntouts/oneliners/main/dive.sh | bash
 ```
+
+## tldr
+
+To install [tealdeer](https://github.com/dbrgn/tealdeer):
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/gntouts/oneliners/main/tldr.sh | bash
+```
---
+++ tldr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Checking latest version for https://github.com/dbrgn/tealdeer..."
+TLDR_VERSION=$(curl -L -s -o /dev/null -w '%{url_effective}' "https://github.com/dbrgn/tealdeer/releases/latest" | grep -oP "v\d+\.\d+\.\d+" | sed 's/v//')
+echo "Found latest version $TLDR_VERSION"
+arch=$(uname -m)
+
+if [ "$arch" == "x86_64" ]; then
+  CPU_ARCH="x86_64-musl"
+elif [ "$arch" == "armv7l" ] || [ "$arch" == "aarch64" ]; then
+  CPU_ARCH="arm-musleabi"
+fi
+
+echo "Downloading Debian package for tldr v$TLDR_VERSION $CPU_ARCH"
+curl -L -o tldr.deb https://github.com/dbrgn/tealdeer/releases/download/v$TLDR_VERSION/tealdeer-linux-$CPU_ARCH
+
+sudo echo "Installing package..."
+sudo dpkg -i ./tldr.deb
+
+sudo apt-get install -f
+rm -fr ./tldr.deb
+echo ""
+echo "Success"
